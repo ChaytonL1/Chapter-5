491fe67d0c8bdc383c5eb224759f2181ea1155d2
Chayton <chaytonlofgren@gmail.com>
2019-09-24 10:32:28 -0500

    gameZone

---
+++ src/Game_zone/RockPaperScissors.java
@@ -0,0 +1,38 @@
+package Game_zone;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class RockPaperScissors {
+
+    public static void main(String[] args)
+    {
+        Random rand = new Random();
+        Scanner input = new Scanner(System.in);
+
+        int choice;
+        int gen = rand.nextInt(3 ) + 1;
+
+
+        System.out.println(" 1 - Rock \n 2 - Paper\n 3 - Scissors \n Choice >>");
+        choice = input.nextInt();
+
+
+    }
+    public static void rockPaperScissors(int gen)
+    {
+        if (gen == 1)
+        {
+
+        }
+        else if (gen == 2)
+        {
+
+        }
+        else if (gen == 3)
+        {
+            
+        }
+
+    }
+}
