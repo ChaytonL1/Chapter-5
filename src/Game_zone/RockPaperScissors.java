658eab7c6b1202d240c2d3205022a1bcecd07582
Chayton <chaytonlofgren@gmail.com>
2019-09-26 09:55:13 -0500

    1

--- src/Game_zone/RockPaperScissors.java
+++ src/Game_zone/RockPaperScissors.java
@@ -5,34 +5,61 @@ import java.util.Scanner;
 
 public class RockPaperScissors {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){
+
+        String result;
+        int user;
         Random rand = new Random();
         Scanner input = new Scanner(System.in);
 
-        int choice;
-        int gen = rand.nextInt(3 ) + 1;
+        result = "Welcome to Rock Paper Scissors";
+        displayPrint(result);
 
+        result = "Choose the following \n1: Rock \n2: Paper \n3: Scissors";
+        displayPrint(result);
 
-        System.out.println(" 1 - Rock \n 2 - Paper\n 3 - Scissors \n Choice >>");
-        choice = input.nextInt();
-
-
+        int r = rand.nextInt(3)+1;
+        user = input.nextInt();
+        if(user == 1){
+            if(r == 1){
+                result = "Tie";
+            }
+            else if(r == 2){
+                result = "Lose";
+            }
+            else{
+                result = "Win";
+            }
+            displayPrint(result);
+        }
+        if(user == 2){
+            if(r == 2){
+                result = "Tie";
+            }
+            else if(r == 3){
+                result = "Lose";
+            }
+            else{
+                result = "Win";
+            }
+            displayPrint(result);
+        }
+        if(user == 3){
+            if(r == 3){
+                result = "Tie";
+            }
+            else if(r == 1){
+                result = "Lose";
+            }
+            else{
+                result = "Win";
+            }
+            displayPrint(result);
+        }
     }
-    public static void rockPaperScissors(int gen)
-    {
-        if (gen == 1)
-        {
 
-        }
-        else if (gen == 2)
-        {
-
-        }
-        else if (gen == 3)
-        {
-
-        }
-
+    public static void displayPrint(String result){
+        System.out.println(result);
     }
+
 }
